183c4f11c545ac7b305ebccf07f014fed99abe99
JavierMorales <jmoralesr@protonmail.com>
2024-01-25 11:12:46 +0100

    Creado componente ranking

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {MainComponent} from "./main/main.component";
 import {CrearTareaComponent} from "./crear-tarea/crear-tarea.component";
 import {InfoTareaComponent} from "./info-tarea/info-tarea.component";
 import {EliminarTareaComponent} from "./eliminar-tarea/eliminar-tarea.component";
+import {RankingComponent} from "./ranking/ranking.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch:'full'},
   { path: 'login', component: LoginComponent},
   { path: 'main', component: MainComponent},
   { path: 'tarea/details/:idTarea', component: InfoTareaComponent},
-  { path: 'main/consultas', component: EliminarTareaComponent}
+  { path: 'main/consultas', component: EliminarTareaComponent},
+  { path: 'main/consultas/ranking', component: RankingComponent}
 ];
