79adfee8ad309ea92eb7a6ee948b00b3bd1bbd90
javimora0 <jmoralesr@protonmail.com>
2024-01-25 21:55:24 +0100

    Añadido material login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,12 +8,12 @@ import {ServicioCompartidoService} from "../services/compartido/servicio-compart
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from "@angular/material/input";
 import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatOption, MatSelect} from "@angular/material/select";
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, MatFormField, MatInput, FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatIconButton, MatSelect, MatOption],
+  imports: [RouterLink, ReactiveFormsModule, MatFormField, MatInput, FormsModule, MatFormFieldModule, MatInputModule, MatIcon, MatIconButton, MatSelect, MatOption, MatButton],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
